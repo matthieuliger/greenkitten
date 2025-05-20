90f476f0d7a905d874396287147998e3239ef47a
l1dtw77@gmail.com <l1dtw77@gmail.com>
2025-05-20 05:10:46 +0000

    Edited Utils

--- server_code/Utils.py
+++ server_code/Utils.py
@@ -9,7 +9,7 @@ from anvil.tables import app_tables
 import anvil.server
 from PyPDF2 import PdfReader
 import io
-
+import traceback
 
 def extract_text_from_pdf_pypdf2(pdf_media):
   if not pdf_media.content_type.startswith("application/pdf"):
@@ -18,7 +18,12 @@ def extract_text_from_pdf_pypdf2(pdf_media):
     print("PDF file")
   try:
     with pdf_media.get_bytes() as f:
-      reader = PdfReader(f)  # No need to wrap in BytesIO
+      try:
+        reader = PdfReader(io.BytesIO(f))  # No need to wrap in BytesIO
+      except Exception as e:
+        print("PdfReader or BytesIO error")
+        print(e)
+      print("extracting pages")
       text = ""
       for page in reader.pages:
         text += page.extract_text() or ""
@@ -26,3 +31,4 @@ def extract_text_from_pdf_pypdf2(pdf_media):
   except Exception as e:
     print("Error trying to convert")
     print(e)
+    traceback.print_exc()
